49b280548de1f68827ae0e9933205d51f1127d98
Mateusz Loskot <mateusz@loskot.net>
2013-07-17 23:35:45 +0100

    Test failing build for FEATURE>2

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
 # make a command fail
-non-existent-cmd
-echo "[log] non-existent-cmd exit $?"
+if FEATURE -gt 2; then
+    echo "FEATURE -gt 2"
+    non-existent-cmd
+    echo "[log] non-existent-cmd exit $?"
+fi
 
 # not expecting to see this
 echo "[log] echo exit $?"

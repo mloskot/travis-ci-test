81672589b98503ba8a70c932ab7315fb94201af5
Mateusz Łoskot <mateusz.loskot@cadcorp.com>
2015-04-22 22:29:41 +0200

    Install apt-fast, required

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -e
 source ${TRAVIS_BUILD_DIR}/bin/ci/common.sh
 
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
-sudo apt-get update -qq
-sudo apt-get install -qq g++-multilib
-sudo apt-get install -qq mingw32
-
-[ -z ${SOCI_TRAVIS_BACKEND+x} ] && exit 0
+sudo add-apt-repository -y ppa:apt-fast/stable
+sudo apt-get update -qq -y
+sudo apt-get install -qq -y apt-fast
+sudo apt-fast install -qq -y g++-multilib mingw32
 
 before_install="${TRAVIS_BUILD_DIR}/bin/ci/before_install_${SOCI_TRAVIS_BACKEND}.sh"
 [ -x ${before_install} ] && ${before_install} || echo "nothing to run"

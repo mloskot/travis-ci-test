9058e9cf1bbfa2265be82f312dee9fc7105f79a9
Mateusz Łoskot <mateusz.loskot@cadcorp.com>
2015-04-22 23:14:45 +0200

    Add apt-fast update

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -5,6 +5,7 @@ sudo add-apt-repository -y ppa:apt-fast/stable
 sudo apt-get update -qq -y
 sudo apt-get install -qq -y apt-fast
 sudo apt-fast install -qq -y g++-multilib mingw32
+sudo apt-fast update -qq -y
 
 before_install="${TRAVIS_BUILD_DIR}/bin/ci/before_install_${SOCI_TRAVIS_BACKEND}.sh"
 [ -x ${before_install} ] && ${before_install} || echo "nothing to run"

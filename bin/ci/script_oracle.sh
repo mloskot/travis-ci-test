0dc00e5b6d3961eb282cfa5a9ce525e635c7d2fa
Mateusz Łoskot <mateusz.loskot@cadcorp.com>
2015-04-23 00:46:37 +0200

    Add sqlplus commands to create test sessions

--- bin/ci/script_oracle.sh
+++ bin/ci/script_oracle.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# Load Oracle environment variables so that we could run `sqlplus`.
+. /usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/oracle_env.sh
+echo "ORACLE_HOME=${ORACLE_HOME}"
+echo "ORACLE_SID=${ORACLE_SID}"
+
+echo "Connecting using username/password"
+echo "SELECT * FROM v$version;" | \
+sqlplus -S -L travis/travis
+
+echo "Connecting using username/password@XE"
+echo "SELECT * FROM v$version;" | \
+sqlplus -S -L travis/travis@XE

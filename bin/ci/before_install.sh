8b7e1f49b39e608f00c724083c9a62577ef94f48
Mateusz Łoskot <mateusz@loskot.net>
2015-04-23 12:33:05 +0200

    Install libboost i386

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -5,6 +5,8 @@ sudo add-apt-repository -y ppa:apt-fast/stable
 sudo apt-get update -qq -y
 sudo apt-get install -qq -y apt-fast
 sudo apt-fast install -qq -y g++-multilib mingw32
+sudo apt-fast install -qq -y libboost-dev:i386 libboost-date-time-dev:i386
+
 sudo apt-fast update -qq -y
 
 before_install="${TRAVIS_BUILD_DIR}/bin/ci/before_install_${SOCI_TRAVIS_BACKEND}.sh"

5c90aba393244e8facd50c61038ae5505914f63e
Mateusz Łoskot <mateusz@loskot.net>
2015-04-28 21:42:30 +0200

    apt-fast update before install

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -4,10 +4,8 @@ source ${TRAVIS_BUILD_DIR}/bin/ci/common.sh
 sudo add-apt-repository -y ppa:apt-fast/stable
 sudo apt-get update -qq -y
 sudo apt-get install -qq -y apt-fast
-sudo apt-fast install -qq -y g++-multilib mingw32
-sudo apt-fast install -qq -y libboost-dev:i386 libboost-date-time-dev:i386
-
 sudo apt-fast update -qq -y
+sudo apt-fast install -qq -y g++-multilib mingw32
 
 before_install="${TRAVIS_BUILD_DIR}/bin/ci/before_install_${SOCI_TRAVIS_BACKEND}.sh"
 if [ -x ${before_install} ]; then

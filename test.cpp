890b88b60c040cea7956a566e29277cef59c846d
Mateusz Łoskot <mateusz@loskot.net>
2016-11-28 11:45:14 +0100

    Check sizeof integral types

--- test.cpp
+++ test.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 int main()
 {
-    std::puts("Test passed");
+    printf("sizeof(int):       %u\n", sizeof(int));
+    printf("sizeof(long int):  %u\n", sizeof(long int));
+    printf("sizeof(long):      %u\n", sizeof(long));
+    printf("sizeof(long long): %u\n", sizeof(long long));
     return 0;
 }

9342df7a7c3f58271bb65fa80263e88fcf564b20
Mateusz Loskot <mateusz@loskot.net>
2019-01-06 19:58:57 +0100

    Tidy up

--- test1.cpp
+++ test1.cpp
@@ -1,9 +1,6 @@
 #include <cstdio>
 int main()
 {
-    printf("sizeof(int):       %u\n", sizeof(int));
-    printf("sizeof(long int):  %u\n", sizeof(long int));
-    printf("sizeof(long):      %u\n", sizeof(long));
-    printf("sizeof(long long): %u\n", sizeof(long long));
+    printf("This is test1\n");
     return 0;
 }

158dcc7f33a8c5cc6535d2310da451d8d4091e46
Mateusz Łoskot <mateusz@loskot.net>
2019-01-07 13:02:29 +0100

    [circle] Link against Boost::filesystem, if found

--- filesystem.cpp
+++ filesystem.cpp
@@ -6,6 +6,12 @@
 #elif __cpp_lib_experimental_filesystem
     #include <experimental/filesystem>
     using fs = std::experimental::filesystem;
+#else
+    #ifdef CI_TEST_HAS_BOOST_FILESYSTEM
+        #define BOOST_FILESYSTEM_VERSION 3
+        #include <boost/filesystem.hpp>
+        using fs = std::filesystem;
+    #endif
 #endif
 
 #include <iostream>
@@ -14,10 +20,12 @@ int main()
 {
     std::cout << "Detecting std::filesystem availability:" << std::endl;
 
-#ifdef __cpp_lib_filesystem
-    std::cout << "<filesystem>" << std::endl;
-#elif __cpp_lib_experimental_filesystem
-    std::cout << "<experimental/filesystem>" << std::endl;
+#if defined( __cpp_lib_filesystem)
+    std::cout << "using <filesystem>" << std::endl;
+#elif defined(__cpp_lib_experimental_filesystem)
+    std::cout << "using <experimental/filesystem>" << std::endl;
+#elif defined(CI_TEST_HAS_BOOST_FILESYSTEM)
+    std::cout << "using <boost/filesystem.hpp>" << std::endl;
 #else
     std::cout << "no <filesystem> support" << std::endl;
 #endif

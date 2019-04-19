7863152b586277d1e7de27e84f0a776a58611f64
Mateusz Łoskot <mateusz@loskot.net>
2019-04-20 00:25:37 +0200

    Add GCC 4.8 and GCC 4.9 tests

--- test1.cpp
+++ test1.cpp
@@ -1,6 +1,12 @@
 #include <cstdio>
+using namespace std;
 int main()
 {
     printf("This is test1\n");
+
+#ifdef __GLIBCXX__
+    printf("XXXXXXXX __GLIBCXX__=%d\n", __GLIBCXX__);
+#endif
+
     return 0;
 }
